670ca05e3408ec9c7d7686e858a1e3b205ddf668
Muhammed-Amr-Cloud <mohamed.amr@avelabs.com>
2020-12-13 16:17:52 +0200

    Using Threads

--- main.cpp
+++ main.cpp
@@ -5,9 +5,10 @@
  *      Author: Muhamed
  */
 #include <iostream>
-#include <thread>
 #include <string>
+#include <thread>
 using namespace std;
+
 
 /* Abstract Class */
 class Actor
@@ -17,32 +18,36 @@ protected:
 	string		   name ;
 	unsigned int count ;
 
-
 public:
+
+	static unsigned int numberOfActorsCreated ;
+
 	/* Default Constructor */
 	Actor()
 	{
 		name  = "Unknown";
 		count = 0 ;
+		numberOfActorsCreated++;
 	}
 
 	/*Overriding*/
-	Actor(string _name ,unsigned int _count )
+	Actor(string _name ,unsigned int _count  )
 	{
 		name =   _name;
 		count = _count;
+		numberOfActorsCreated++;
 	}
 
 	/*Destructor */
 	virtual ~Actor(){
-		// Do nothing
+		numberOfActorsCreated--;
 	}
 
-	/* Here we made this class an Abstract Class By using Pure Virtual Function */
-	virtual void decrease () = 0  ;
-	virtual void increase ()  = 0 ;
+	/* Here we made this class an Abstract Class By using virtual Function */
+	/* if i dont want to create objects from Actor I could use Pure Virtual Function */
 
-
+	virtual void decrease () {}; //do nothing
+	virtual void increase () {}; //do nothing
 
 	/*setters and Getters*/
 
@@ -56,8 +61,19 @@ public:
 		count = value;
 	}
 
+	/***/
+
+	unsigned int getNumberofActorsCreated(){
+
+		return numberOfActorsCreated;
+	}
+
+	string getName(){return name;}
+	void  setName(string _name ) {name = _name;}
 };
 
+
+unsigned int Actor::numberOfActorsCreated = 0  ;
 
 class DefaultCount:public Actor{
 
@@ -169,19 +185,49 @@ public:
 
 
 
-
-
-
-
-
-
-int main()
+int main(int argc, char* argv[])
 {
 
-	DefaultCount   D("mohamed",0);
-	CountWithValue V("mohamed",0,3);
-	AutoIncrease   A ("mohamed",0,3,6);
+	Actor actorArr[10] ;
+	int flag  = 0 ;
 
+	thread actorThread(Actor); // seperate thread
+	if(argc > 1)
+	{
+		for(int i = 1 ; i < argc ;i++)
+		{
+
+			for(int j = 0 ; j < i; j++)
+			{
+
+				// if multiple names, count only one
+				if(actorArr[j].getName() == argv[i])
+				{
+					flag = 1 ;
+				}
+
+			}
+				if(flag == 1)
+				{
+					flag = 0 ;
+
+				}
+				else
+				{
+					 actorArr[i].setName(argv[i]);
+					cout<<actorArr[i].getName()<<" "<<endl;
+				}
+		}
+	}
+	else
+	{
+
+
+	}
+
+	DefaultCount   D("mohamed",0);
+	CountWithValue V("Layla",0,3);
+	AutoIncrease   A ("Peter",0,3,6);
 
 	cout<<D.getCount()<<" ";
 	D.increase();
@@ -194,7 +240,9 @@ int main()
 	cout<<V.getCount()<<" "<<endl;
 
 	//A.increase();
-	cout<<A.getCount();
+	cout<<A.getCount()<<endl;
+	cout<< A.getNumberofActorsCreated();
+
 
     return 0;
 }
